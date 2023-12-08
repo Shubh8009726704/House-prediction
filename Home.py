eceae0f5cdf1a3436f2e0fc9384a2ba736b8a8fb
Shubham Sharma <121294706+Shubh8009726704@users.noreply.github.com>
2023-12-08 11:48:10 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,34 +11,33 @@ if st.sidebar.button('Home'):
     st.markdown('''<h1 style='color:black;background:yellow;padding:15px;border-radius:10px'>Predicting the price of a house in Ramanthapur(Hyderabad).</h1>''',unsafe_allow_html=True)
 
     st.markdown('''<h3>Model to predict the price of a house based on its features</h3>''',unsafe_allow_html=True)
+    st.header('House features')
+    col1, col2, col3, col4= st.columns(4)
+    with col1:
+        area = st.slider('Area: ', 1650, 16200, 50)
+        bedrooms = st.radio('No. of bedrooms: ', [1, 2, 3, 4, 5, 6],horizontal=True)
+        bathrooms = st.radio('No. of bathrooms: ', [1, 2, 3, 4],horizontal=True)
+    with col2:
+        stories = st.radio('No. of stories: ', [1, 2, 3, 4],horizontal=True)
+        parking = st.radio('Parking: ', [0, 1, 2, 3],horizontal=True)
+        mainroad = st.radio('Main road: ', [0, 1],horizontal=True)
+    with col3: 
+        guestroom = st.radio('Guest room: ', [0, 1],horizontal=True)
+        basement = st.radio('Basement: ', [0, 1],horizontal=True)
+        hotwaterheating = st.radio('Hot water heating: ', [0, 1],horizontal=True)
+    with col4:
+        airconditioning = st.radio('Air conditioning: ', [0, 1],horizontal=True)
+        prefarea = st.radio('Preferred area: ', [0, 1],horizontal=True)
+        furnishingstatus = st.radio('Furnishing status: furn, Semi-Fur, Unfurnished ', [0, 1, 2],horizontal=True)
 
-st.header('House features')
-col1, col2, col3, col4= st.columns(4)
-with col1:
-    area = st.slider('Area: ', 1650, 16200, 50)
-    bedrooms = st.radio('No. of bedrooms: ', [1, 2, 3, 4, 5, 6],horizontal=True)
-    bathrooms = st.radio('No. of bathrooms: ', [1, 2, 3, 4],horizontal=True)
-with col2:
-    stories = st.radio('No. of stories: ', [1, 2, 3, 4],horizontal=True)
-    parking = st.radio('Parking: ', [0, 1, 2, 3],horizontal=True)
-    mainroad = st.radio('Main road: ', [0, 1],horizontal=True)
-with col3: 
-    guestroom = st.radio('Guest room: ', [0, 1],horizontal=True)
-    basement = st.radio('Basement: ', [0, 1],horizontal=True)
-    hotwaterheating = st.radio('Hot water heating: ', [0, 1],horizontal=True)
-with col4:
-    airconditioning = st.radio('Air conditioning: ', [0, 1],horizontal=True)
-    prefarea = st.radio('Preferred area: ', [0, 1],horizontal=True)
-    furnishingstatus = st.radio('Furnishing status: furn, Semi-Fur, Unfurnished ', [0, 1, 2],horizontal=True)
-
-if st.button("Predict Price of House"):
-    data = np.array([area, bedrooms	, bathrooms	, stories, parking, mainroad, guestroom, basement, hotwaterheating, airconditioning, prefarea, furnishingstatus])
-    data = pd.DataFrame(data.reshape(-1, len(data)), columns=['area', 'bedrooms', 'bathrooms', 'stories', 'parking','mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea', 'furnishingstatus'])
-    st.write(data)
-    prediction = predict(data)
-    st.header('Predicted price of house is : Rs. ' + str(round(prediction[0],2)))
+    if st.button("Predict Price of House"):
+        data = np.array([area, bedrooms	, bathrooms	, stories, parking, mainroad, guestroom, basement, hotwaterheating, airconditioning, prefarea, furnishingstatus])
+        data = pd.DataFrame(data.reshape(-1, len(data)), columns=['area', 'bedrooms', 'bathrooms', 'stories', 'parking','mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea', 'furnishingstatus'])
+        st.write(data)
+        prediction = predict(data)
+        st.header('Predicted price of house is : Rs. ' + str(round(prediction[0],2)))
     
-st.success('Note : If you are facing any problem related to my App contact with me.')
+    st.success('Note : If you are facing any problem related to my App contact with me.')
 
 if st.sidebar.button('Contact us'):
     st.write('<h2>Name : Shubham Sharma</h2>',unsafe_allow_html=True)
